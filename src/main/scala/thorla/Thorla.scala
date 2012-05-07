1dda2d4443123fe1fb4a03c7ae7a0a076f99e7db
Jon Crussell <joncrussell@gmail.com>
2012-05-07 16:12:59 -0700

    added some accessor methods to options, added some more documentation

--- src/main/scala/thorla/Thorla.scala
+++ src/main/scala/thorla/Thorla.scala
@@ -71,6 +71,7 @@ trait Thorla {
    * Returns the usage for a specific subcommand or lists the available subcommands
    *
    * @param invoke: specify a subcommand to print usage for, by default prints all subcommands and descriptions
+   * @return usage string
    */
   final def usage(invoke: String = ""): String = {
     if(invoke == "" || !subCommands.contains(invoke)) { // List the subcommands
@@ -88,6 +89,12 @@ trait Thorla {
     }
   }
 
+  /**
+   * Tests whether this Thorla responds to the given task
+   *
+   * @param task: a task of the form namespace:invoke to test for
+   * @return true iff this thorla can run the given task
+   */
   final def respondsTo(task: String): Boolean = {
     subCommands.keys.find(sub => {
       "%s:%s".format(namespace, sub) == task
@@ -98,6 +105,7 @@ trait Thorla {
    * Builds the actual option parser for options
    *
    * @param invoke: name of the subcommand to build options for, must be in subCommands
+   * @return OptionParser for the given invoke subcommand
    */
   private def buildOptionParser(invoke: String): OptionParser = {
     val parser = new OptionParser(invoke)
@@ -142,14 +150,13 @@ trait Thorla {
     parser
   }
 
-  private def updateArg(index: Int, value: String) {
-    argValues(index) = value
-  }
-
-  private def updateOption(name: String, value: Any) {
-    options += (name -> value)
-  }
-
+  /**
+   * Invoke a task, should make sure this Thorla respondsTo the task first
+   *
+   * @param task: a task of the form namespace:invoke to start
+   * @param args: additional arguments from the command line
+   * @return return value of the subcommand or non-zero if error
+   */
   final def invoke(task: String, args: Array[String]): Int = {
     val invoke = task.split(":")(1)
     val parser = buildOptionParser(invoke)
@@ -174,6 +181,33 @@ trait Thorla {
       Console.err.println("Failed to parse args")
       1
     }
+  }
+
+  /*
+   * Accessor methods for option values, helps with typing
+   *
+   * As long as the application programmer knows what they are doing, this won't cause the application to
+   * crash...
+   */
+
+  def optionAsString(name: String): String = {
+    options(name).asInstanceOf[String]
+  }
+
+  def optionAsInt(name: String): Int = {
+    options(name).asInstanceOf[Int]
+  }
+
+  def optionAsDouble(name: String): Double = {
+    options(name).asInstanceOf[Double]
+  }
+
+  private def updateArg(index: Int, value: String) {
+    argValues(index) = value
+  }
+
+  private def updateOption(name: String, value: Any) {
+    options += (name -> value)
   }
 
   private def getMethod(name: String): Option[Method] = {
